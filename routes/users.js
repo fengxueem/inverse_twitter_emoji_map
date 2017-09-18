0c4c1c8b5cb5d64c8df6f885261e67cdb5b44e89
fengxueem <fengxueem@sina.com>
2017-09-18 17:49:04 +0800

    first commit

---
+++ routes/users.js
@@ -0,0 +1,105 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb');
+var Twit = require('twit');
+
+
+//connect to database
+var mdbServer = mongo.Server('localhost', 27017, { 'auto_reconnect': true });
+var mdb = mongo.Db('streaming_db_v', mdbServer);
+
+mdb.open(function(err, db) {
+    if (!err) { console.log('Connected to streaming_db_v@localhost:27017'); }
+    db.createCollection('tweets', function(err, collection) {
+        var tweetCount = 0;
+        var firstTweet = true;
+
+        //add neccessary attributes to tweets and insert them into database.
+        function processTweets(tweet) {
+            try {
+                if (tweetCount < 1000000) {
+                    var parsedTweet = tweet;
+                    if (parsedTweet.id && parsedTweet.id_str) {
+                        parsedTweet._id = new mongo.Long.fromString(parsedTweet.id_str, 10);
+                        parsedTweet._createdDate = new Date(parsedTweet.created_at);
+                        parsedTweet._negEmoji = getNegEmoji(parsedTweet.text);
+                        parsedTweet._posEmoji = getPosEmoji(parsedTweet.text);
+                        collection.insertOne(parsedTweet, function(err, result) {
+                            if (err) {
+                                console.log("Error writing document to database. Most likely a duplicate.");
+                            } else {
+                                if (result.insertedCount === 1) {
+                                    //console.log(tweetCount + " tweets saved:" + "; " + tweet.text);
+                                }
+                            }
+                        });
+                        tweetCount++;
+                    }
+                } else {
+                    db.close();
+                    process.exit(0);
+                }
+            } catch (e) {
+                console.log("Exception thrown:" + e.message)
+            }
+        }
+        //define sentiment
+        function getNegEmoji(text) {
+            var negEmojis = ('😶,😐,😑,😒,🙄,🤔,😳,😞,😟,😠,😡,😔,😕,🙁,☹️,😣,😖,😫,😩,😤,😮,😱,😨,😰,😯,😦,😧,😢,😥,😪,😓,😭,😵,🤐,😷,🤒,🤕,😴,🙀,😿,😾,🐵,🙈,🙉,🙊').split(',');
+            var negEmojiOccurance = 0;
+            if (text) {
+                // Each emoji consists of two characters.
+                for (var i = 0; i < negEmojis.length; i += 1) {
+                    var char = negEmojis[i];
+                    if (isEmoji(char, text)) {
+                        negEmojiOccurance++;
+                    }
+                }
+                // console.log('neg' + text + negEmojiOccurance);
+                return negEmojiOccurance;
+            }
+        }
+
+        function getPosEmoji(text) {
+            var posEmojis = ('😀,😬,😁,😂,😃,😄,😅,😆,😇,😉,😊,🙂,🙃,☺️,😋,😌,😍,😘,😗,😙,😚,😜,😝,😛,🤑,🤓,😎,🤗,😏,💩,😈,👿,😺,😸,😹,😻,😼,😽').split(',');
+            var posEmojiOccurance = 0;
+            if (text) {
+                // Each emoji consists of two characters.
+                for (var i = 0; i < posEmojis.length; i += 1) {
+                    var char = posEmojis[i];
+                    if (isEmoji(char, text)) {
+                        posEmojiOccurance++;
+                    }
+                }
+                // console.log('pos' + text + posEmojiOccurance);
+                return posEmojiOccurance;
+            }
+        }
+
+        function isEmoji(char, emojis) {
+            if (emojis.indexOf(char) > -1) {
+                return true;
+            }
+        }
+
+        //connecting to twitter
+        var T = new Twit({
+            consumer_key: '6h6GrZeFSVEsr3gFKbAO2Q5A1',
+            consumer_secret: 'b0EzhxDxJYVFcfU3xEtD1nrViMrjCb18JRhigaYKvpnhKrHuRE',
+            access_token: '851864518993092610-pq1DIanzIcQ7Jaovy3DkCZ4Rm7cXpQZ',
+            access_token_secret: 'R3dJTtQToBtUkAgFlFCMrnIrMD7r5GM9C9hTKyRwx66uR',
+            timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+        });
+
+        var emojis = '😀,😬,😁,😂,😃,😄,😅,😆,😇,😉,😊,🙂,🙃,☺️,😋,😌,😍,😘,😗,😙,😚,😜,😝,😛,🤑,🤓,😎,🤗,😏,😶,😐,😑,😒,🙄,🤔,😳,😞,😟,😠,😡,😔,😕,🙁,☹️,😣,😖,😫,😩,😤,😮,😱,😨,😰,😯,😦,😧,😢,😥,😪,😓,😭,😵,🤐,😷,🤒,🤕,😴,💩,😈,👿,😺,😸,😹,😻,😼,😽,🙀,😿,😾,🐵,🙈,🙉,🙊';
+        var stream = T.stream('statuses/filter', { track: emojis });
+        stream.on('tweet', processTweets);
+    });
+});
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+module.exports = router;
